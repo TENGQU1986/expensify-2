c0a96a5ace9c2993e55ff7b000e8ecd51badaed7
Patrick Qu <quteng.bo@gmail.com>
2018-02-07 15:34:06 -0500

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import './styles/styles.scss';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter /> , document.getElementById('app'));
+const store = configureStore();
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
