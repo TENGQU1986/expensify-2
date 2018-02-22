9130148ae28a0433018aa4dffa16b796adbd5082
Patrick Qu <quteng.bo@gmail.com>
2018-02-22 16:29:18 -0500

    jest test for components

---
+++ src/tests/components/ExpenseList.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExpenseList from '../../components/ExpenseList';
+import expenses from '../fixtures/expenses';
+
+test('shoudl render ExpenseList with expenses', () => {
+  const wrapper = shallow(<ExpenseList expense={expenses}/>);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('shoudl render ExpenseList with empty expenses', () => {
+  const wrapper = shallow(<ExpenseList expense={[]}/>);
+  expect(wrapper).toMatchSnapshot();
+});
