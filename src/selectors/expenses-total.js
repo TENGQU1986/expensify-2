7ce12862a22d42ee58fb5f2993ccbb2c5b63d96d
Patrick Qu <quteng.bo@gmail.com>
2018-03-08 01:35:33 -0500

    finish all test

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,7 +1,13 @@
-import expenses from '../fixtures/expenses';
-
 export default (expenses) => {
-  return expenses.map((expense) => expense.amount).reduce((sum, value) => {
-    return sum + value;
-  }, 0)
+  return expenses
+      .map((expense) => expense.amount)
+      .reduce((sum, value) => sum + value, 0);
 };
+
+// import expenses from '../fixtures/expenses';
+//
+// export default (expenses) => {
+//   return expenses.map((expense) => expense.amount).reduce((sum, value) => {
+//     return sum + value;
+//   }, 0)
+// };
