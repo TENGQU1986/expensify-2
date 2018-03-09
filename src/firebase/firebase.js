9eb29b9f368dba9da71da4481b8198f4e72530db
Patrick Qu <quteng.bo@gmail.com>
2018-03-09 12:41:03 -0500

    firebase-1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,48 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAapA5P8gWC83aHK26xgDDMzIBC4dw914w",
-  authDomain: "expensify-a13e6.firebaseapp.com",
-  databaseURL: "https://expensify-a13e6.firebaseio.com",
-  projectId: "expensify-a13e6",
-  storageBucket: "expensify-a13e6.appspot.com",
-  messagingSenderId: "124728096317"
-};
+    apiKey: "AIzaSyD7HIH_CCSEMcBLlHp4c_wRQ1sk4OcYNGQ",
+    authDomain: "expensify-61793.firebaseapp.com",
+    databaseURL: "https://expensify-61793.firebaseio.com",
+    projectId: "expensify-61793",
+    storageBucket: "expensify-61793.appspot.com",
+    messagingSenderId: "227548607778"
+  };
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Patrick'
+const database = firebase.database();
+
+database.ref().on('value', (snapshot) => {
+  console.log(snapshot.val());
 });
+
+// database.ref().set({
+//   name: 'Patrick',
+//   age: 30,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Kearny',
+//     country: 'US'
+//   }
+// }).then(() => {
+//   console.log('data is seved');
+// }).catch((e) => {
+//   console.log('This failed', e);
+// });
+//
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+
+// database.ref('isSingle').remove().then(() => {
+//   console.log('Remove succeeded');
+// }).catch((e) => {
+//   console.log('Removed failed', e);
+// })
