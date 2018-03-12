533397204a85666a43c36353ca019cea05eeb2fe
Patrick Qu <quteng.bo@gmail.com>
2018-03-12 14:58:34 -0400

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyD7HIH_CCSEMcBLlHp4c_wRQ1sk4OcYNGQ",
-    authDomain: "expensify-61793.firebaseapp.com",
-    databaseURL: "https://expensify-61793.firebaseio.com",
-    projectId: "expensify-61793",
-    storageBucket: "expensify-61793.appspot.com",
-    messagingSenderId: "227548607778"
+    apiKey: "process.env.FIREBASE_API_KEY",
+    authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
+    databaseURL: "process.env.FIREBASE_DATABASE_URL",
+    projectId: "process.env.FIREBASE_PROJECT_ID",
+    storageBucket: "process.env.FIREBASE_STORAGE_BUCKET",
+    messagingSenderId: "process.env.FIREBASE_MESSAGEING_SENDER_ID"
   };
 
 firebase.initializeApp(config);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if(process.env.NODE_ENV === 'test') {
+  require('dotene').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotene').config({ path: '.env.development' });
+}
+
+//process.env.NODE_ENV
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -37,7 +48,20 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
+      new webpack,DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+
+        'process.env.FIREBASE_MESSAGEING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGEING_SENDER_ID)
+      });
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
