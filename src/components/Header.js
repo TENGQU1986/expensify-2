17a8d49fd06a3648bc71b1d19cb9b0e021e9cdd2
Patrick Qu <quteng.bo@gmail.com>
2018-03-16 14:54:50 -0400

    Public Route added

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,9 @@ export const Header = ({startLogout}) => {
   return(
     <header>
       <h1>Expensify</h1>
-      <NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+      <NavLink to="/dashboard" activeClassName="is-active" >Dashboard</NavLink>
       <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
       {/* <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> */}
-      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
       <button onClick={startLogout}>Logout</button>
     </header>
   );
