908f614a4e2433acd2099863351836fa10f30c4a
Patrick Qu <quteng.bo@gmail.com>
2018-03-20 02:30:08 -0400

    whole polished

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -56,9 +56,9 @@ export default class ExpenseForm extends React.Component {
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+        
+        <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error" >{this.state.error}</p>}
           <input
             className="text-input"
             type="text"
@@ -89,9 +89,10 @@ export default class ExpenseForm extends React.Component {
             onChange={this.onNoteChange}
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+            <button className="login-button ">Save Expense</button>
+          </div>
         </form>
-      </div>
-    )
+    );
   }
 }
